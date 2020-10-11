27ca6a36af21d1d976eebeffee00d78ef3ad6d46
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-10-11 12:38:51 +0300

    Add insertion sort algorithm

--- Algorithms/Program.fs
+++ Algorithms/Program.fs
@@ -4,6 +4,6 @@
 let main argv =
     // For testing
     let list = [| 2; 45; 59; 1; 0; -2; 5; 19 |]
-    for item in GnomeSort.Sort list do
+    for item in InsertionSort.Sort list do
         System.Console.WriteLine item
     0
---
+++ Algorithms/Sort/Insertion_Sort.fs
@@ -0,0 +1,13 @@
+﻿namespace Sort
+
+module InsertionSort =
+    let Sort list: 'T[] =
+        let mutable list = list |> Array.copy
+        for index in 1..list.Length - 1 do
+            let item = list.[index]
+            let mutable j = index
+            while j > 0 && list.[j - 1] > item do
+                list.[j] <- list.[j - 1]
+                j <- j - 1
+            list.[j] <- item
+        list
