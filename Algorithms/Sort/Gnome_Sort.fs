db0f976fdcb6a6d515834e7cbbae493858c74cbb
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-10-11 12:27:06 +0300

    Add gnome sort algorithm

---
+++ Algorithms/Sort/Gnome_Sort.fs
@@ -0,0 +1,20 @@
+﻿namespace Sort
+
+module GnomeSort =
+    let Sort list: 'T[] =
+        let mutable list = list |> Array.copy
+        let mutable first = 1
+        let mutable second = 2
+        while first < list.Length do
+            if list.[first - 1] <= list.[first] then
+                first <- second
+                second <- second + 1
+            else
+                let tmp = list.[first - 1]
+                list.[first - 1] <- list.[first]
+                list.[first] <- tmp
+                first <- first - 1
+                if first = 0 then
+                    first <- 1
+                    second <- 2
+        list
