770b7ecfea68c0eb5e5266ba0d5eefc853b62d00
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-10-11 09:08:48 +0300

    Add bubble sort algorithm

---
+++ Algorithms/Program.fs
@@ -0,0 +1,9 @@
+﻿open Sort
+
+[<EntryPoint>]
+let main argv =
+    // For testing
+    let list = [| 2; 45; 59; 1; 0; -2; 5; 19 |]
+    for item in BubbleSort.Sort(list) do
+        System.Console.WriteLine item
+    0
---
+++ Algorithms/Sort/Bubble_Sort.fs
@@ -0,0 +1,17 @@
+﻿namespace Sort
+
+module BubbleSort =
+    let rec Sort list: 'T[] =
+        let mutable updated = false
+        let mutable list = list |> Array.copy
+        for index in 0..list.Length - 1 do
+            if index < list.Length - 1 then
+                let current = list.[index]
+                let next = list.[index + 1]
+                if next < current then
+                    list.[index] <- next
+                    list.[index + 1] <- current
+                    updated <- true
+        if updated then
+            list <- Sort list
+        list
