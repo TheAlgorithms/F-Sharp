018ac7809368cc57e2d54186e79ac911331acdb5
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-10-11 09:31:48 +0300

    Add comb sort algorithm

---
+++ Algorithms/Sort/Comb_Sort.fs
@@ -0,0 +1,22 @@
+﻿namespace Sort
+
+module CombSort =
+    let Sort list: 'T[] =
+        let mutable list = list |> Array.copy
+        let mutable gap = double list.Length
+        let mutable swaps = true
+        while gap > 1.0 || swaps do
+            gap <- gap / 1.247330950103979
+            if gap < 1.0 then
+                gap <- 1.0
+            let mutable i = 0
+            swaps <- false
+            while i + int gap < list.Length do
+                let igap = i + int gap
+                if list.[i] > list.[igap] then
+                    let swap = list.[i]
+                    list.[i] <- list.[igap]
+                    list.[igap] <- swap
+                    swaps <- true
+                i <- i + 1
+        list
