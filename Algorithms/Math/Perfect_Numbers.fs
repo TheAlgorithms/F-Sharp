7c30c914a3626d303f6f90e49983ffb86afbff98
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-10-23 22:06:19 +0300

    Create Perfect_Numbers.fs

---
+++ Algorithms/Math/Perfect_Numbers.fs
@@ -0,0 +1,10 @@
+namespace Math
+
+module Perfect_Numbers =
+    let IsPerfect (number: int) =
+        let mutable total = 0
+        for num in 1..number - 1 do
+            if number % num = 0 then
+                total <- total + number
+        let res = total = number
+        res
