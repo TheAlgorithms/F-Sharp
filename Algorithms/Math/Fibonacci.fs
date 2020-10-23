e9ecab4b35aa1204144098bead0b1d8663a2df1c
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-10-23 21:47:03 +0300

    Add fibonacci.

---
+++ Algorithms/Math/Fibonacci.fs
@@ -0,0 +1,7 @@
+﻿namespace Math
+
+module Fibonacci =
+    let rec PrintSerie (one: int) (two: int) =
+        let fibo = one + two
+        System.Console.WriteLine fibo
+        PrintSerie two fibo
--- Algorithms/Program.fs
+++ Algorithms/Program.fs
@@ -1,9 +1,12 @@
-﻿open Sort
+﻿open global.Math
+open Sort
 
 [<EntryPoint>]
 let main argv =
-    // For testing
-    let list = [| 2; 45; 59; 1; 0; -2; 5; 19 |]
-    for item in InsertionSort.Sort list do
-        System.Console.WriteLine item
+    // For sort algorihm testing
+    //let list = [| 2; 45; 59; 1; 0; -2; 5; 19 |]
+    //for item in InsertionSort.Sort list do
+    //    System.Console.WriteLine item
+    Fibonacci.PrintSerie 1 0
+    
     0
