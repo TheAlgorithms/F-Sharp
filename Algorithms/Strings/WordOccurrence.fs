3ab8dfd14f76399ff05320c7dd3f22c5581a15af
powpow58 <41553768+powpow58@users.noreply.github.com>
2020-12-11 15:43:54 -0400

    Added word occurrence

---
+++ Algorithms/Strings/WordOccurrence.fs
@@ -0,0 +1,12 @@
+﻿namespace Strings
+open System.Collections.Generic
+
+module WordOccurence =
+    let wordOccurence (sentence : string) =
+        // Creating a dictionary containing count of each word
+        let occurence = new Dictionary<string, int>()
+        for word in sentence.Split(" ") do
+            match word with
+            | w when occurence.ContainsKey(w) -> occurence.Item(w) <- occurence.GetValueOrDefault(w) + 1
+            | _ -> occurence.Add(word, 1)
+        occurence
