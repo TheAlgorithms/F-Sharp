d193ffe9dcb2a4b39249b707edee3f1037d65677
powpow58 <41553768+powpow58@users.noreply.github.com>
2020-12-11 15:03:45 -0400

    Added Reverse Letters

---
+++ Algorithms/Strings/ReverseLetters.fs
@@ -0,0 +1,13 @@
+﻿namespace Strings
+
+module ReverseLetters =
+    /// <summary>
+    /// Reverses letters in a given string without adjusting the position of the words
+    /// </summary>
+    /// <param name="input">String to reverse.</param>
+    /// <returns>Reversed string</returns>
+    let reverseLetters(input: string) =
+        let mutable str = ""
+        for i in input do
+            str <- str.Insert(0, i.ToString())
+        str
