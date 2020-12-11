90ca3e3cc097defac7d2248df3e6ae0119752203
powpow58 <41553768+powpow58@users.noreply.github.com>
2020-12-11 13:42:48 -0400

    Added BinarySearch to Search Folder and Split to Strings

---
+++ Algorithms/Strings/Split.fs
@@ -0,0 +1,14 @@
+﻿namespace Strings
+
+module Split =
+    let split(str: string, separator: string) =
+        let mutable newStringList = []
+        let mutable value = ""
+        if str.Contains separator then
+            for c in str do
+                match c.ToString() with
+                | c when c.Contains separator -> newStringList <- newStringList |> List.append <| [value]; value <- ""
+                | _ -> value <- value + c.ToString(); 
+            if value <> "" then newStringList <- newStringList |> List.append <| [value]; value <- ""
+        else newStringList <- [value]
+        newStringList
