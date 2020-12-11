7ae7b19558e7af55f8517060e14d6a5ae7e13f74
powpow58 <41553768+powpow58@users.noreply.github.com>
2020-12-11 17:57:32 -0400

    Added remove duplicates in Strings

---
+++ Algorithms/Strings/RemoveDuplicates.fs
@@ -0,0 +1,11 @@
+﻿namespace Strings
+
+open Microsoft.FSharp.Collections
+
+module RemoveDuplicates =
+    let removeDuplicates (string: string) =
+        let mutable newString = ""
+        for s in string.Split() do
+            if not (newString.Contains s) then
+                newString <- newString + " " + s
+        newString
