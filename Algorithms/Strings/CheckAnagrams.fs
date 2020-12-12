8bc47b121fed35d7a6ac4866be20abb6bd099bd6
powpow58 <41553768+powpow58@users.noreply.github.com>
2020-12-12 16:39:01 -0400

    Added check anagrams

---
+++ Algorithms/Strings/CheckAnagrams.fs
@@ -0,0 +1,20 @@
+﻿// wiki: https://en.wikipedia.org/wiki/Anagram
+
+namespace Algorithms.Strings
+
+module CheckAnagrams =
+    /// <summary>
+    /// Two strings are anagrams if they are made of the same letters
+    /// arranged differently (ignoring the case).
+    /// </summary>
+    /// <param name="string1">First string</param>
+    /// <param name="string1">Second string</param>
+    /// <returns>Boolean</returns>
+    let isAnagram (string1: string, string2: string) =
+        let a =
+            string1.ToCharArray() |> Array.sortBy (id)
+
+        let b =
+            string2.ToCharArray() |> Array.sortBy (id)
+
+        a = b
