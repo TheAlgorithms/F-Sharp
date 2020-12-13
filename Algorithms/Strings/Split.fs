82e091bd9159fc9c40aeb1d9b5317225326c51e2
powpow58 <41553768+powpow58@users.noreply.github.com>
2020-12-13 08:34:16 -0400

    Update Split.fs

--- Algorithms/Strings/Split.fs
+++ Algorithms/Strings/Split.fs
@@ -5,7 +5,7 @@ module Split =
     /// Will split the string up into all the values separated by the separator
     /// </summary>
     /// <param name="str">String to split into lists</param>
-    /// <param name="item">The separator on what you would like to separate your strings</param>
+    /// <param name="separator">The separator on what you would like to separate your strings</param>
     /// <returns>A string list</returns>
     let split (str: string, separator: string) =
         let mutable newStringList = []
