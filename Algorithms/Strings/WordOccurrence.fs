a2ffa7d1a0c393e28add1bd0c7d705b465ee7327
powpow58 <41553768+powpow58@users.noreply.github.com>
2020-12-14 10:10:43 -0400

    Update WordOccurrence.fs

--- Algorithms/Strings/WordOccurrence.fs
+++ Algorithms/Strings/WordOccurrence.fs
@@ -2,12 +2,12 @@
 
 open System.Collections.Generic
 
-module WordOccurence =
-    let wordOccurence (sentence: string) =
+module WordOccurrence =
+    let wordOccurrence (sentence: string) =
         // Creating a dictionary containing count of each word
-        let occurence = new Dictionary<string, int>()
+        let occurrence = new Dictionary<string, int>()
         for word in sentence.Split(" ") do
             match word with
-            | w when occurence.ContainsKey(w) -> occurence.Item(w) <- occurence.GetValueOrDefault(w) + 1
-            | _ -> occurence.Add(word, 1)
-        occurence
+            | w when occurrence.ContainsKey(w) -> occurrence.Item(w) <- occurrence.GetValueOrDefault(w) + 1
+            | _ -> occurrence.Add(word, 1)
+        occurrence
