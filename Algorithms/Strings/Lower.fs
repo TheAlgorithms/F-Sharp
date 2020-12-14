34ecd5201fbc8efbd027688d111c650ea51d60df
powpow58 <41553768+powpow58@users.noreply.github.com>
2020-12-14 18:25:09 -0400

    Update Lower.fs

--- Algorithms/Strings/Lower.fs
+++ Algorithms/Strings/Lower.fs
@@ -8,28 +8,28 @@ module Lower =
     /// <code>
     /// lower("wow")
     /// 'wow'
-    /// 
+    ///
     /// lower("HellZo")
     /// hellzo'
-    /// 
+    ///
     /// lower("WHAT")
     /// 'what'
-    /// 
+    ///
     /// lower("wh[]32")
     /// 'wh[]32'
-    /// 
+    ///
     /// lower("whAT")
     /// 'what'
     /// </code>
     /// </example>
     /// <param name="word"></param>
     /// <returns></returns>
-    let lower(word:string):string =
+    let lower (word: string): string =
         let mutable str = ""
         // converting to ascii value int value and checking to see if char is a capital
         for letter in word do
-                // letter if it is a capital letter it is getting shift by 32 which makes it a lower case letter
-                if letter >= 'A' && letter <= 'Z'
-                then str <- str + (string) ((char) ((int) letter + 32))
-                else str <- str + (string) letter
+            // letter if it is a capital letter it is getting shift by 32 which makes it a lower case letter
+            if letter >= 'A' && letter <= 'Z'
+            then str <- str + (string) ((char) ((int) letter + 32))
+            else str <- str + (string) letter
         str
