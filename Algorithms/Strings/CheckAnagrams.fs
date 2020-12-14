258f095cb4eb3582f9358cf8f2f12476a5ff4cbb
powpow58 <41553768+powpow58@users.noreply.github.com>
2020-12-14 09:44:39 -0400

    Update CheckAnagrams.fs

--- Algorithms/Strings/CheckAnagrams.fs
+++ Algorithms/Strings/CheckAnagrams.fs
@@ -1,12 +1,26 @@
-﻿// wiki: https://en.wikipedia.org/wiki/Anagram
+﻿namespace Algorithms.Strings
 
-namespace Algorithms.Strings
-
+/// wiki: https://en.wikipedia.org/wiki/Anagram
 module CheckAnagrams =
     /// <summary>
     /// Two strings are anagrams if they are made of the same letters
     /// arranged differently (ignoring the case).
     /// </summary>
+    /// <example>
+    /// <code>
+    /// check_anagrams('Silent', 'Listen')
+    /// True
+    /// 
+    /// check_anagrams('This is a string', 'Is this a string')
+    /// True
+    /// 
+    /// check_anagrams('This is    a      string', 'Is     this a string')
+    /// True
+    /// 
+    /// check_anagrams('There', 'Their')
+    /// False
+    /// </code>
+    /// </example>
     /// <param name="string1">First string</param>
     /// <param name="string2">Second string</param>
     /// <returns>Boolean</returns>
