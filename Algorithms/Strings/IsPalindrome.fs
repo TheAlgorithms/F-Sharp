72a1010b44e7e7660e308c62e837795a2721a866
powpow58 <41553768+powpow58@users.noreply.github.com>
2021-01-01 10:48:58 -0400

    Update IsPalindrome.fs

--- Algorithms/Strings/IsPalindrome.fs
+++ Algorithms/Strings/IsPalindrome.fs
@@ -4,8 +4,8 @@ module IsPalindrome =
     /// <summary>
     /// Determine whether the string is palindrome
     /// </summary>
-    /// <param name="s"></param>
+    /// <param name="str"></param>
     /// <returns>Boolean</returns>
-    let isPalindrome (s: string): bool =
+    let isPalindrome (str: string): bool =
         let str = str |> Seq.filter ((<>) ' ') |> Seq.toList
         str = (str |> List.rev)
