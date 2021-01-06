2cc7c60d124f02bb677a8716d3eb85bde1d09a5f
Allister Isaiah Harvey <isaiahharvey1998@outlook.com>
2021-01-06 18:39:47 -0400

    Updated ReverseLettersTests

--- Algorithms.Tests/Strings/ReverseLettersTests.fs
+++ Algorithms.Tests/Strings/ReverseLettersTests.fs
@@ -10,7 +10,7 @@ type ReverseLettersTests () =
     [<DataRow("The cat in the hat", "ehT tac ni eht tah")>]
     [<DataRow("The quick brown fox jumped over the lazy dog.", "ehT kciuq nworb xof depmuj revo eht yzal .god")>]
     [<DataRow("Is this true?", "sI siht ?eurt")>]
-    [<DataRow("I   love       Python", "I evol nohtyP")>]
+    [<DataRow("I   love       F#", "I   evol       #F")>]
     member this.reverseLetters (input:string, expected:string) =
         let actual = ReverseLetters.reverseLetters(input)
         Assert.AreEqual(expected, actual)
