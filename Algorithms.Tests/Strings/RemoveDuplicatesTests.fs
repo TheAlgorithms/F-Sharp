6ffec52b11a84709e99d8c23adc04f75398d9209
Allister Isaiah Harvey <isaiahharvey1998@outlook.com>
2021-01-06 18:33:51 -0400

    Updated RemoveDeplicatesTests

--- Algorithms.Tests/Strings/RemoveDuplicatesTests.fs
+++ Algorithms.Tests/Strings/RemoveDuplicatesTests.fs
@@ -7,8 +7,8 @@ open Algorithms.Strings
 type RemoveDuplicatesTests () =
 
     [<TestMethod>]
-    [<DataRow("Python is great and Java is also great", "Java Python also and great is")>]
-    [<DataRow("Python   is      great and Java is also great", "Java Python also and great is")>]
+    [<DataRow("Python is great and Java is also great", "Python is great and Java also")>]
+    [<DataRow("Python   is      great and Java is also great", "Python  is great and Java also")>]
     member this.removeDuplicates (str:string, expected:string) =
         let actual = RemoveDuplicates.removeDuplicates(str)
         Assert.AreEqual(expected, actual)
