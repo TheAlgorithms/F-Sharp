04ad356474d2cc65819f41e07d07c83588eac467
Allister Isaiah Harvey <isaiahharvey1998@outlook.com>
2021-01-17 19:12:26 -0400

    Jaro Winkler

--- Algorithms.Tests/Strings/JaroWinklerTests.fs
+++ Algorithms.Tests/Strings/JaroWinklerTests.fs
@@ -7,12 +7,13 @@ open Algorithms.Strings
 type JaroWinklerTests () =
     [<TestMethod>]
     [<DataRow("martha", "marhta", 0.9611111111111111)>]
-    [<DataRow("CRATE", "TRACE", 0.8483333333333334)>]
+    [<DataRow("CRATE", "TRACE", 0.7333333333333334)>]
     [<DataRow("test", "dbdbdbdb", 0.0)>]
-    [<DataRow("hello world", "HeLLo W0rlD", 0.7131313131313132)>]
+    [<DataRow("test", "test", 1.0)>]
+    [<DataRow("hello world", "HeLLo W0rlD", 0.6363636363636364)>]
     [<DataRow("test", "", 0.0)>]
-    [<DataRow("hello", "world", 0.49)>]
-    [<DataRow("hell**o", "*world", 0.373015873015873)>]
+    [<DataRow("hello", "world", 0.4666666666666666)>]
+    [<DataRow("hell**o", "*world", 0.4365079365079365)>]
     member this.jaroWinkler (str1:string, str2:string, expected:float) =
         let actual = JaroWinkler.jaroWinkler(str1, str2)
         Assert.AreEqual(expected, actual)
