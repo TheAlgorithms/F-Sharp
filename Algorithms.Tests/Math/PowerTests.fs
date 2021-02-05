17754ad7dcdfb7a0eb2962b0ed6f57fa7b2d95ed
Liam Di Dato <liamdidato@gmail.com>
2021-02-05 20:57:26 +1000

    Added Factorial and Power, plus tests (#13)

---
+++ Algorithms.Tests/Math/PowerTests.fs
@@ -0,0 +1,22 @@
+﻿namespace Algorithms.Tests.Math
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Algorithms.Math
+
+[<TestClass>]
+type PowerTests () =
+    
+    [<TestMethod>]
+    [<DataRow(2, 2, 4)>]
+    [<DataRow(2, 3, 8)>]
+    [<DataRow(2, 4, 16)>]
+    [<DataRow(2, 8, 256)>]
+    [<DataRow(2, 16, 65536)>]
+    [<DataRow(3, 5, 243)>]
+    [<DataRow(5, 3, 125)>]
+    [<DataRow(10, 4, 10000)>]
+    [<DataRow(1, 2, 1)>]
+    [<DataRow(1, 50, 1)>]
+    member this.PowerOf (num: int, pow: int, expected: int) =
+        let actual = Power.Pow num pow
+        Assert.AreEqual(expected, actual)
