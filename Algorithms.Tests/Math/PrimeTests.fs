0eafd474be2e87407be8a6a8c32ed1015341d075
i1e0 <96235137+i1e0@users.noreply.github.com>
2022-01-06 00:23:38 +0900

    Add Prime and its tests

---
+++ Algorithms.Tests/Math/PrimeTests.fs
@@ -0,0 +1,22 @@
+﻿namespace Algorithms.Tests.Math
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Algorithms.Math
+
+[<TestClass>]
+type PrimeTests() =
+
+    [<TestMethod>]
+    [<DataRow(-3, false)>]
+    [<DataRow(-2, false)>]
+    [<DataRow(-1, false)>]
+    [<DataRow(0, false)>]
+    [<DataRow(1, false)>]
+    [<DataRow(2, true)>]
+    [<DataRow(49, false)>]
+    [<DataRow(50, false)>]
+    [<DataRow(998244353, true)>]
+    [<DataRow(1000000007, true)>]
+    member this.IsPrime(input: int, expected: bool) =
+        let actual = Prime.isPrime input
+        Assert.AreEqual(expected, actual)
---
+++ Algorithms/Math/Prime.fs
@@ -0,0 +1,20 @@
+﻿namespace Algorithms.Math
+
+module Prime =
+    /// Check if a number is prime. Time complexity: O(√n)
+    let isPrime n =
+        if n <= 1 then
+            false
+        elif n = 2 then
+            true
+        elif n % 2 = 0 then
+            false
+        else
+            seq {
+                let mutable i = 3
+
+                while i * i <= n do
+                    yield i
+                    i <- i + 2
+            }
+            |> Seq.forall ((%) n >> (<>) 0)
