8cef8117b60f659b8764f6508c15ef76e3ce94e3
i1e0 <96235137+i1e0@users.noreply.github.com>
2022-01-06 00:24:09 +0900

    Add tests for math functions

---
+++ Algorithms.Tests/Math/AbsMaxTests.fs
@@ -0,0 +1,13 @@
+﻿namespace Algorithms.Tests.Math
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Algorithms.Math
+
+[<TestClass>]
+type AbsMaxTests() =
+
+    [<TestMethod>]
+    member this.Test() =
+        Assert.AreEqual(-3, AbsMax.absMax [ -1; 1; 2; -2; -3; 3 ])
+        Assert.AreEqual(3, AbsMax.absMax [ 1; -1; 2; -2; 3; -3 ])
+        Assert.AreEqual(-1, AbsMax.absMax [ 0; -1; 0 ])
---
+++ Algorithms.Tests/Math/AbsTests.fs
@@ -0,0 +1,17 @@
+﻿namespace Algorithms.Tests.Math
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Algorithms.Math
+
+[<TestClass>]
+type AbsTests() =
+
+    [<TestMethod>]
+    [<DataRow(-10, 10)>]
+    [<DataRow(-1, 1)>]
+    [<DataRow(0, 0)>]
+    [<DataRow(1, 1)>]
+    [<DataRow(10, 10)>]
+    member this.Test(input: int, expected: int) =
+        let actual = Abs.absVal input
+        Assert.AreEqual(expected, actual)
