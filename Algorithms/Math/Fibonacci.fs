f2fda0cc88675f64d589aaf3da9988b798734c05
Hogosha <77451110+Hogosha42@users.noreply.github.com>
2022-08-01 15:57:07 +0200

    update Fibonacci.fs
    added a function to get the nth Fibonacci number.

--- Algorithms/Math/Fibonacci.fs
+++ Algorithms/Math/Fibonacci.fs
@@ -1,7 +1,13 @@
-﻿namespace Algorithms.Math
+namespace Algorithms.Math
 
 module Fibonacci =
     let rec PrintSerie (one: int) (two: int) =
         let fibo = one + two
         System.Console.WriteLine fibo
         PrintSerie two fibo
+        
+    let rec NthFibonacci (n: int): int =
+        match n with
+            | 0 -> 0
+            | 1 -> 1
+            | n -> NthFibonacci (n-1) + NthFibonacci (n-2)
