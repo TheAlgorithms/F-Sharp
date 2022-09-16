cd837b0c2cc85a7a7a8fccea875b2d8e4b32dec5
Mertcan D <54983926+mertcandav@users.noreply.github.com>
2022-09-16 15:34:40 +0300

    Create HasSuffix.fs

---
+++ HasSuffix.fs
@@ -0,0 +1,6 @@
+module HasSuffix =
+    /// <summary>
+    /// Reports string has specified suffix or not.
+    /// </summary>
+    let HasSuffix (s: string, suffix: string) =
+        s.Length >= suffix.Length && s.Substring(s.Length-suffix.Length) = suffix
