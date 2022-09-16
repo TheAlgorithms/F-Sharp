d2d7ea4bc9b175fbc034a21f340b5729d0003340
Mertcan D <54983926+mertcandav@users.noreply.github.com>
2022-09-16 15:31:24 +0300

    Create HasPrefix.fs

---
+++ Algorithms/Strings/HasPrefix.fs
@@ -0,0 +1,7 @@
+module HasPrefix =
+    /// <summary>
+    /// Reports string has specified prefix or not.
+    /// </summary>
+    let HasPrefix (s: string, prefix: string): bool =
+        s.Length >= prefix.Length && s.Substring(0, prefix.Length) = prefix
+ 
