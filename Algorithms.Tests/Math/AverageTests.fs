d7c62a2cd22bc38179d00724497310d7f1d9f1ea
Mark Shevchenko <markshevchenko@gmail.com>
2022-10-08 17:37:28 +0300

    Implement average function and tests

---
+++ Algorithms.Tests/Math/AverageTests.fs
@@ -0,0 +1,15 @@
+﻿namespace Algorithms.Tests.Math
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Algorithms.Math
+
+[<TestClass>]
+type AverageTests() =
+
+    [<TestMethod>]
+    member this.Test() =
+        Assert.AreEqual(None, Average.average List.empty<float>)
+        Assert.AreEqual(Some 2.0, Average.average [1.0; 2.0; 3.0])
+        Assert.AreEqual(Some 1.0, Average.average [1.0])
+
+
