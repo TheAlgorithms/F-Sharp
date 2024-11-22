c835b74bff29be90649a6dd2a2de56c53c3f396c
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-11-22 18:58:00 +0600

    Uncomment tests

--- Algorithms.Tests/Strings/MinCostStringConversionTests.fs
+++ Algorithms.Tests/Strings/MinCostStringConversionTests.fs
@@ -3,21 +3,20 @@
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open Algorithms.Strings
 
-// FIXME
-// [<TestClass>]
-// type MinCostStringConversionTests () =
+[<TestClass>]
+type MinCostStringConversionTests () =
 
-//     [<TestMethod>]
-//     [<DataRow("abbbaba", "abbba")>]
-//     [<DataRow("ababa", "ababa")>]
-//     member this.assembleTransformation (ops:string list, i:int, j:int, expected:string list) =
-//         let actual = MinCostStringConversion.assembleTransformation(ops, i, j)
-//         Assert.AreEqual(expected, actual)
+    [<TestMethod>]
+    [<DataRow("abbbaba", "abbba")>]
+    [<DataRow("ababa", "ababa")>]
+    member this.assembleTransformation (ops:string list, i:int, j:int, expected:string list) =
+        let actual = MinCostStringConversion.assembleTransformation(ops, i, j)
+        Assert.AreEqual(expected, actual)
 
-//     [<TestMethod>]
-//     [<DataRow("abbbaba", "abbba")>]
-//     [<DataRow("ababa", "ababa")>]
-//     member this.assembleTransformation (sourceString:string, destinationString:string, copyCost:int, replaceCost:int, deleteCost:int, insertCost:int, expected:int list * string list) =
-//         let actual = MinCostStringConversion.computeTransformTables(sourceString,destinationString,copyCost,replaceCost,deleteCost,insertCost)
-//         Assert.AreEqual(expected, actual)
+    [<TestMethod>]
+    [<DataRow("abbbaba", "abbba")>]
+    [<DataRow("ababa", "ababa")>]
+    member this.assembleTransformation (sourceString:string, destinationString:string, copyCost:int, replaceCost:int, deleteCost:int, insertCost:int, expected:int list * string list) =
+        let actual = MinCostStringConversion.computeTransformTables(sourceString,destinationString,copyCost,replaceCost,deleteCost,insertCost)
+        Assert.AreEqual(expected, actual)
 
