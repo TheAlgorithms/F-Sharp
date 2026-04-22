b8cf2e98a72b2225130421c903002a0fbbf97d73
Mertcan D. <54983926+mertcandav@users.noreply.github.com>
2026-04-22 12:57:10 +0300

    math/perfect_numbers: update deprecated approaches

--- Algorithms/Math/Perfect_Numbers.fs
+++ Algorithms/Math/Perfect_Numbers.fs
@@ -6,7 +6,7 @@ module PerfectNumbers =
         match n with
         | n when n <= 0 -> false
         | n ->
-            { 1 .. n - 1 }
+            seq { 1 .. n - 1 }
             |> Seq.takeWhile (fun i -> i * i <= n)
             |> Seq.filter ((%) n >> (=) 0)
             |> Seq.fold (fun acc i -> acc + i + n / i) 0
